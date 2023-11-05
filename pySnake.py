a4eb77ca2ee8c97d353bbcb8787a1346667dc6e4
izevar <antonio_di_maria@me.com>
2023-11-05 22:32:39 +0100

    fruit class

--- pySnake.py
+++ pySnake.py
@@ -1,9 +1,21 @@
 #PySnake - Antonio Di Maria 2023
 #this is a simple project i did use to learn pygame 
 #Tested on python 3.9 with pygame 2.5
-import pygame
-import sys
+import pygame, sys, random
+from pygame.math import Vector2
 
+class FRUIT:
+    def __init__(self):
+        #x and y pos
+        self.x = random.randint(0, CELL_NUMBER-1)
+        self.y = random.randint(0, CELL_NUMBER-1)
+        self.pos = Vector2(self.x, self.y)
+
+    #draw fruit
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x*CELL_SIZE), int(self.pos.y*CELL_SIZE), CELL_SIZE, CELL_SIZE)
+        pygame.draw.rect(screen, FRUIT_COLOR, fruit_rect)
+        
 #init pygame
 pygame.init()
 
@@ -13,6 +25,7 @@ FPS_CAP = 60
 #RGB touples
 BACKGROUND_COLOR = (175,215,70)
 SURFACE_COLOR = (0,0,255)
+FRUIT_COLOR = (126,166,144)
 
 #virtual grid
 CELL_SIZE = 40
@@ -28,6 +41,9 @@ screen = pygame.display.set_mode((CELL_SIZE*CELL_NUMBER, CELL_SIZE*CELL_NUMBER))
 #create a clock to limit game speed to FPS_CAP
 clock = pygame.time.Clock()
 
+#game objects
+fruit = FRUIT()
+
 #game loop
 while True:
     #event loop
@@ -37,5 +53,6 @@ while True:
             sys.exit()
 
     screen.fill(BACKGROUND_COLOR)
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(FPS_CAP)
